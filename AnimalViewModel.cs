28c354b4ca83e40f7b2fdba7c8e51bf9902acbc0
opilane <opilane@HP-CZC2349HTC>
2026-04-28 12:44:48 +0300

    Add project files.

---
+++ AnimalViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MauiApp1
+{
+
+
+
+    public class AnimalViewModel : INotifyPropertyChanged
+    {
+        private string _currentAnimalImage = string.Empty;
+
+        public string CurrentAnimalImage
+        {
+            get => _currentAnimalImage;
+            set
+            {
+                _currentAnimalImage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public AnimalViewModel()
+        {
+            // Kui AppResources.resx on projekti juurkaustas:
+            CurrentAnimalImage = AppResources.AnimalCat;
+        }
+
+        public void ChangeAnimal(string type)
+        {
+            CurrentAnimalImage = type switch
+            {
+                "Dog" => AppResources.AnimalDog,
+                "Fish" => AppResources.AnimalFish,
+                _ => AppResources.AnimalCat
+            };
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+
+
+
+
+
+}
---
+++ MainPage.cs
@@ -0,0 +1,35 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    readonly AnimalViewModel vm = new AnimalViewModel();
+
+    public MainPage()
+    {
+        BindingContext = vm;
+
+        var image = new Image
+        {
+            HeightRequest = 200,
+            WidthRequest = 200
+        };
+        image.SetBinding(Image.SourceProperty, nameof(vm.CurrentAnimalImage));
+
+        var btnCat = new Button { Text = "Näita Kassi", HorizontalOptions = LayoutOptions.Fill };
+        var btnDog = new Button { Text = "Näita Koera", HorizontalOptions = LayoutOptions.Fill };
+        var btnFish = new Button { Text = "Näita Kala", HorizontalOptions = LayoutOptions.Fill };
+
+        btnCat.Clicked += (s, e) => vm.ChangeAnimal("Cat");
+        btnDog.Clicked += (s, e) => vm.ChangeAnimal("Dog");
+        btnFish.Clicked += (s, e) => vm.ChangeAnimal("Fish");
+
+        Content = new VerticalStackLayout
+        {
+            Spacing = 20,
+            Padding = new Thickness(30),
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.Center,
+            Children = { image, btnCat, btnDog, btnFish }
+        };
+    }
+}
